222ff92e051ec806553006c192ba0d0e4f1644a4
jules testard <jules.testard@gmail.com>
2014-11-20 21:30:51 -0800

    simpler parser

---
+++ src/test/java/edu/uci/ics/asterix/sqlpp/parser/SQLPPTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package edu.uci.ics.asterix.sqlpp.parser;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author julestestard
+ *
+ */
+public class SQLPPTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		String string = "1+(2+3)*4";
+		InputStream stringStream = new java.io.ByteArrayInputStream(string.getBytes());
+		try {
+			SQLPP parser = new SQLPP(stringStream);
+			Exp result = parser.S();
+			System.out.println(result);
+		} catch (ParseException e) {
+	        System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+}
